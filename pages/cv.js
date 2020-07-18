d255322e5c505fdf232643c3304f5bc205b381ba
Thomas O'Neill <tom@bearjam.dev>
2020-07-18 16:07:03 +0100

    linked pdf from web version

--- pages/cv.js
+++ pages/cv.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import SvgEnvelope from '../components/SvgEnvelope';
-import SvgGithub from '../components/SvgGithub';
-import SvgLink from '../components/SvgLink';
-import SvgLinkedin from '../components/SvgLinkedin';
-import ResumeMD from '../content/resume.md';
-import Head from 'next/head';
-import { MDXProvider } from '@mdx-js/react';
-import css from '../styles/cv.module.css';
+import React from "react"
+import SvgEnvelope from "../components/SvgEnvelope"
+import SvgGithub from "../components/SvgGithub"
+import SvgLink from "../components/SvgLink"
+import SvgLinkedin from "../components/SvgLinkedin"
+import ResumeMD from "../content/resume.md"
+import Head from "next/head"
+import { MDXProvider } from "@mdx-js/react"
+import css from "../styles/cv.module.css"
+import SvgDescription from "../components/SvgDescription"
 
 export default () => {
   return (
@@ -17,16 +18,32 @@ export default () => {
       <header className={css.header}>
         <h1>Thomas O'Neill</h1>
         <ul id="social-links">
-          <li><SvgLink /><a href="https://tom.bearjam.dev/cv">tom.bearjam.dev/cv</a></li>
-          <li><SvgEnvelope /><a href="mailto:tom@bearjam.dev">tom@bearjam.dev</a></li>
-          <li><SvgGithub /><a href="https://github.com/mysterybear">mysterybear</a></li>
-          <li><SvgLinkedin /><a href="https://www.linkedin.com/in/tomatbearjam">linkedin.com/in/tomatbearjam</a></li>
+          <li>
+            <SvgEnvelope />
+            <a href="mailto:tom@bearjam.dev">tom@bearjam.dev</a>
+          </li>
+          <li>
+            <SvgGithub />
+            <a href="https://github.com/mysterybear">mysterybear</a>
+          </li>
+          <li>
+            <SvgLinkedin />
+            <a href="https://www.linkedin.com/in/tomatbearjam">
+              linkedin.com/in/tomatbearjam
+            </a>
+          </li>
+          <li>
+            <SvgDescription />
+            <a href="https://tom.bearjam.dev/thomas-oneill-cv-2020-07.pdf">
+              View as PDF
+            </a>
+          </li>
         </ul>
+        <p>React developer; systems engineer.</p>
         <p>
-          React developer; systems engineer.
-        </p>
-        <p>
-          Lover of the declarative, functional style;<br/>Unix philosophy; interactive learning.
+          Lover of the declarative, functional style;
+          <br />
+          Unix philosophy; interactive learning.
         </p>
       </header>
       <article className={css.article}>
@@ -35,5 +52,5 @@ export default () => {
         </MDXProvider>
       </article>
     </div>
-  );
+  )
 }
