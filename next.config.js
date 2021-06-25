e81e4cb8161afd8b93510ef0fdba24240bc00a7c
Tom Bear <tom@bearjam.dev>
2021-06-25 16:39:38 +0100

    try no webpack 5

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
-});
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+})
 
-const withFonts = require('next-fonts');
+const withFonts = require("next-fonts")
 
-module.exports = withFonts(withMDX({
-  webpack: (config, options) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    }
-    return config
-  },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-}));
+module.exports = withFonts(
+  withMDX({
+    webpack: (config, options) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: "empty",
+      }
+      return config
+    },
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    future: {
+      webpack5: false,
+    },
+  })
+)
